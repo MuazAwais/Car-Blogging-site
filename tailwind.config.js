41f929b5d508ffd780cfdfee37f288a916e1341c
muazawais <muazawais1947@gmail.com>
2025-08-31 23:13:54 +0500

    shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+ /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-		 fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-      },
-       animation: {
-          'slide-in-left': 'slideInFromLeft 1s ',
-        },
-      keyframes: {
-          slideInFromLeft: {
-            '0%': { transform: 'translateX(-100%)' },
-            '100%': { transform: 'translateX(0)' },
-          },
-        },
-	},
-  },
-  plugins: [],
-}
-
+   content: ["./src/**/*.{html,js}"],
+   theme: {
+     extend: {},
+   },
+   plugins: [],
+ }
